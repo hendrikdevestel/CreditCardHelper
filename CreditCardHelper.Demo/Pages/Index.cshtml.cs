6a058465c8be9a19c3029c4c9b0cf54b64f50499
Hendrik <hendrikdevestel@gmail.com>
2022-05-23 22:07:32 +0200

    add demo web project

---
+++ CreditCardHelper.Demo/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CreditCardHelper.Demo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Text { get; set; }
+        public bool CreditCardFound { get; set; }
+        public string MatchingText { get; set; }
+
+        public IndexModel()
+        {
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            CreditCardFound = Validator.TextContainsCreditCard(Text);
+            if(CreditCardFound)
+                MatchingText = Validator.GetCardNumberFromText(Text);
+        }
+    }
+}
